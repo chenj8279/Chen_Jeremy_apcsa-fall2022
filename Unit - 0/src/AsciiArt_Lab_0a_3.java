26c23aed14c21fb5cdbca3d26ebab0294b399b7d
chenj8279 <111385353+chenj8279@users.noreply.github.com>
2022-08-18 23:04:08 -0700

    Add files via upload

---
+++ Unit - 0/src/AsciiArt_Lab_0a_3.java
@@ -0,0 +1,40 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Jeremy Chen
+//Date - 8/18/22
+//Class - Mauro; P2
+//Lab  - AsciiArt Lab 0a 3
+
+public class AsciiArt_Lab_0a_3
+{
+	public static void main ( String[] args )
+	{
+		System.out.println("Jeremy Chen \n\n" );
+		System.out.println("bear" );
+		System.out.println("\n\n\n\n" );
+// change all \ to \\
+		System.out.println("       _---_              _---_              " );
+		System.out.println("     /       \\          /       \\         " );
+		System.out.println("    |   xxx    \\______/    xxx   |          " );
+		System.out.println("     \\                          /           " );
+		System.out.println("      )                        (               " );
+		System.out.println("     /      xx            xx    \\              " );
+		System.out.println("    |       xx            xx     |             " );
+		System.out.println("    |            _____           |            " );
+		System.out.println("     \\           \\   /          /            " );
+		System.out.println("      \\       x   \\ /   x      /              " );
+		System.out.println("       |        xx   xx       |                  " );
+		System.out.println("        |                    |                     " );
+		System.out.println("        |                    |            " );
+		System.out.println("                                          " );
+		System.out.println("                                          " );
+		
+		//add other output
+
+		System.out.println(" \n\n\n\nHelpFul Hints" );
+		System.out.println("\\\\ draws one backslash on the screen!\n" );
+		System.out.println("\\\" draws one double quote on the screen!\n" );
+		System.out.println("\\\' draws one single quote on the screen!\n" ); 
+	}
+}
---
+++ Unit - 0/src/Varibles_lab_0b_1.java
@@ -0,0 +1,65 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Jeremy Chen
+//Date - 8/18/22
+//Class - Mauro, P2
+//Lab  - Varibles Lab 0b 1
+
+public class Varibles_lab_0b_1
+{
+	public static void main ( String[] args )
+	{
+		//define 1 variable of each of the
+		//following data types
+		//byte		short		int 		long
+		//float		double
+		//char      boolean		String
+
+		//integer variables
+		byte byteOne = 127;
+		short smol = 52;
+		int num = 187;
+		long tree = 2000187358;
+		float boat = 66;
+		double pong = 88.445;
+		char letter = 65;
+		boolean rather = false;
+		String thread = "yarn";
+		
+		
+
+
+		//decimal variables
+
+
+		//other integer types
+
+
+		//other types
+
+
+		//output your information here
+		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+		System.out.println("*16 bit - short = "+smol+"\t\t*");
+		System.out.println("*32 bit - int = "+num+"\t\t*");
+		System.out.println("*64 bit - long = "+tree+"\t\t*");
+		System.out.println("*32 bit - float = "+boat+"\t\t*");
+		System.out.println("*64 bit - double = "+pong+"\t\t*");
+		System.out.println("*16 bit - char = "+letter+"\t\t*");
+		System.out.println("*bits - boolean = "+rather+"\t\t*");
+		System.out.println("*bits - string = "+thread+"\t\t*");
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
