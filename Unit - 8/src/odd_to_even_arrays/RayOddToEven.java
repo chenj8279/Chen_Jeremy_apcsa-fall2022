41e2d34eb6d076f65f9048e39f8d5e389264aec0
chenj8279 <111385353+chenj8279@users.noreply.github.com>
2022-09-12 11:15:23 -0700

    Add files via upload

--- Unit - 8/src/odd_to_even_arrays/RayOddToEven.java
+++ Unit - 8/src/odd_to_even_arrays/RayOddToEven.java
@@ -6,9 +6,20 @@ package odd_to_even_arrays;
 
 public class RayOddToEven
 {
-	public static boolean go(int[] ray)
+	public static int go(int[] ray)
 	{
-		
-		return false;
+		int span = 0;
+		for(int i = 0; i < ray.length; i++) {
+			if(ray[i]%2 == 1) {
+				for(int j = i+1; j < ray.length; j++) {
+					
+					if(ray[j] % 2 ==0) {
+						span = j-i;
+						return span;
+					}
+				}
+			}
+		}
+		return -1;
 	}
 }
