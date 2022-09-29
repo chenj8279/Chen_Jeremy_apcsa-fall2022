ea4e2ef87e16c72400c70e17490559f96e7230ea
chenj8279 <111385353+chenj8279@users.noreply.github.com>
2022-09-29 11:18:50 -0700

    Add files via upload

--- Unit - 11/src/ToyStore.java
+++ Unit - 11/src/ToyStore.java
@@ -24,26 +24,51 @@ public class ToyStore
 		for(int i = 0; i < tys.length; i++) {
 			out.println(tys[i]);
 			for(int j = 0; j < toyList.size(); j++) {
-				//if() {
-					
-				//}
+				Toy temp1 = toyList.get(j);
+				Toy temp2 = toyList.get(i);
+				if(temp1.getName().equals(temp2.getName())) {
+					temp1.setCount(temp1.getCount());
+				}
+				else if (j == toyList.size()-1) {
+					toyList.add(new Toy(tys[i]));
+				}
 			}
 		}
 	}
   
   	public Toy getThatToy( String nm )
   	{
+  		for(int i = 0; i < toyList.size(); i++) {
+  			Toy temp1 = toyList.get(i);
+			if(temp1.getName().equals(nm)) {
+				return toyList.get(i);
+			}
+  		}
   		return null;
   	}
   
   	public String getMostFrequentToy()
   	{
-  		return "";
+  		sortToysByCount();
+  		return "" + toyList.get(0);
   	}  
   
   	public void sortToysByCount()
   	{
-  		
+  		List<Integer> tempC = new ArrayList<Integer>();
+  		List<Toy> tempT = new ArrayList<Toy>();
+  		for (int i = 0; i < toyList.size(); i++) {
+  			tempC.add(toyList.get(i).getCount());
+  		}
+  		Collections.sort(tempC);
+  		Collections.reverse(tempC);
+  		for(int i = 0; i < toyList.size(); i++) {
+  			for(int j = 0; j != toyList.get(i).getCount(); j++) {
+  				if(toyList.get(i).getCount() == tempC.get(j)) {
+  					
+  				}
+  			}
+  		}
   	}  
   	  
 	public String toString()
