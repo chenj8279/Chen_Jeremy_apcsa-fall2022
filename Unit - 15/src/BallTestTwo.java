2ac761696ba6f3225c66c8734d0465ed6fa205fa
chenj8279 <111385353+chenj8279@users.noreply.github.com>
2022-11-04 11:40:08 -0700

    Add files via upload

--- Unit - 15/src/BallTestTwo.java
+++ Unit - 15/src/BallTestTwo.java
@@ -12,21 +12,21 @@ import java.awt.event.ActionListener;
 class BallTestTwo extends Canvas implements Runnable
 {
 	private Ball ball;
-
+	
 	public BallTestTwo()
 	{
 		setBackground(Color.WHITE);
 		setVisible(true);
-
+		
 		//instantiate a new Ball
-
-
+		
+		
 		//test the Ball thoroughly
 		
-
+		
 		//test all constructors
-
-
+		
+		
 		new Thread(this).start();
 	}
 	
@@ -34,34 +34,31 @@ class BallTestTwo extends Canvas implements Runnable
 	{
 		paint(window);
 	}
-
+	
 	public void paint(Graphics window)
 	{
 		ball.moveAndDraw(window);
-
+		
 		if(!(ball.getX()>=10 && ball.getX()<=550))
 		{
 			ball.setXSpeed(-ball.getXSpeed());
 		}
-
+		
 		if(!(ball.getY()>=10 && ball.getY()<=450))
 		{
 			ball.setYSpeed(-ball.getYSpeed());
 		}
 	}
 	
-   public void run()
-   {
-   	try
-   	{
-   		while(true)
+	public void run() {
+		try {
+			while(true)
    		{
    		   Thread.currentThread().sleep(19);
             repaint();
-         }
-      }catch(Exception e)
-      {
-    	  
-      }
-  	}	
+        }
+		}catch(Exception e) {
+			
+		}
+	}	
 }
